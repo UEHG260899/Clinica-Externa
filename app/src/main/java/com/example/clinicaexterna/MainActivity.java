e0a683cfc23fa857d71712553fc011636de57d6e
Uriel <urielenrique99@gmail.com>
2020-12-02 16:27:18 -0600

    Se agrego funcionalidad al registro

--- app/src/main/java/com/example/clinicaexterna/MainActivity.java
+++ app/src/main/java/com/example/clinicaexterna/MainActivity.java
@@ -6,26 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button btnLogin;
+    EditText etEmail, etPass;
+    TextView tvRegistro;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inicializarComponentes();
 
+    }
+
+    private void inicializarComponentes(){
         btnLogin = findViewById(R.id.btnLogin);
+        etEmail = findViewById(R.id.etCorreoL);
+        etPass = findViewById(R.id.etEmailR);
+        tvRegistro = findViewById(R.id.tvRegistro);
 
+        btnLogin.setOnClickListener(this);
+        tvRegistro.setOnClickListener(this);
+    }
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DrawerClinica.class);
-                startActivity(intent);
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btnLogin: {
+
+                break;
             }
-        });
+            case R.id.tvRegistro: {
+                startActivity(new Intent(MainActivity.this, RegistroActivity.class));
+                break;
+            }
+
+        }
     }
 }
