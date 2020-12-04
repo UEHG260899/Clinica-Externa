bf14773a3b4b571909bb16c0b97092451b559ac0
Uriel <urielenrique99@gmail.com>
2020-12-03 18:32:40 -0600

    Se agrego la funcion de crear un paciente

--- app/src/main/java/com/example/clinicaexterna/MainActivity.java
+++ app/src/main/java/com/example/clinicaexterna/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.clinicaexterna;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +10,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.clinicaexterna.mdbf.Recepcionista;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -17,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText etEmail, etPass;
     TextView tvRegistro;
     FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +41,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         inicializarComponentes();
+        iniciarFirebase();
 
+    }
+
+
+    private void iniciarFirebase(){
+        FirebaseApp.initializeApp(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Recepcionistas");
     }
 
     private void inicializarComponentes(){
@@ -43,7 +69,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
 
             case R.id.btnLogin: {
+                String email = etEmail.getText().toString();
+                String pass = etPass.getText().toString();
+                if(email.isEmpty() || pass.isEmpty()){
+                    etEmail.setError("Este campo es obligatorio");
+                    etPass.setError("Este campo es obligatorio");
+                }else{
+                    firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                databaseReference.orderByChild("correo").equalTo(email).addChildEventListener(new ChildEventListener() {
+                                    @Override
+                                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                                        Recepcionista recep = snapshot.getValue(Recepcionista.class);
+                                        Intent intent = new Intent(MainActivity.this, DrawerClinica.class);
+                                        intent.putExtra("nombre", recep.getNombre());
+                                        intent.putExtra("correo", recep.getCorreo());
+                                        startActivity(intent);
+                                    }
 
+                                    @Override
+                                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                                    }
+
+                                    @Override
+                                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                                    }
+
+                                    @Override
+                                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+                            }else{
+                                try {
+                                    throw task.getException();
+                                }catch(FirebaseAuthInvalidUserException ex){
+                                    etEmail.requestFocus();
+                                    etEmail.setError("El usuario no esta registrado");
+                                }catch(FirebaseAuthInvalidCredentialsException ex){
+                                    etPass.requestFocus();
+                                    etPass.setError("La contraseña no coincide con el registro");
+                                }catch(Exception ex){
+                                    Toast.makeText(MainActivity.this, "Ocurrio un error desconocido", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    });
+                }
                 break;
             }
             case R.id.tvRegistro: {
---
+++ app/src/main/java/com/example/clinicaexterna/mdbf/Paciente.java
@@ -0,0 +1,109 @@
+package com.example.clinicaexterna.mdbf;
+
+public class Paciente {
+
+    private String id;
+    private String nombre;
+    private String fecha;
+    private String edad;
+    private String peso;
+    private String estatura;
+    private String img;
+    private String sexo;
+    private String doctor;
+    private String area;
+    private String recep;
+
+
+    public Paciente(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(String estatura) {
+        this.estatura = estatura;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getRecep() {
+        return recep;
+    }
+
+    public void setRecep(String recep) {
+        this.recep = recep;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+}
