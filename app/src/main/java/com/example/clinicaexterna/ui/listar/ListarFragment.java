4eded144348d4a2f7e8cf60731b5075e1314d86c
Uriel <urielenrique99@gmail.com>
2020-12-04 13:29:45 -0600

    Se agrego funcionalidad al fragment listar

--- app/src/main/java/com/example/clinicaexterna/ui/listar/ListarFragment.java
+++ app/src/main/java/com/example/clinicaexterna/ui/listar/ListarFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +16,28 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.clinicaexterna.R;
+import com.example.clinicaexterna.mdbf.Paciente;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListarFragment extends Fragment {
 
     private ListarViewModel listarViewModel;
+    ListView lvPacientes;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseUser user;
+
+    private List<Paciente> listaPaciente = new ArrayList<>();
+    ArrayAdapter<Paciente> arrayAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +51,44 @@ public class ListarFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        iniciarFirebase();
+        iniciarComponentes(root);
+        listarDatos();
+
         return root;
+    }
+
+
+    private void iniciarFirebase(){
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Pacientes");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    private void iniciarComponentes(View root){
+        lvPacientes = root.findViewById(R.id.lvPacientes);
+    }
+
+    private void listarDatos(){
+            databaseReference.orderByChild("recep").equalTo(user.getEmail()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    listaPaciente.clear();
+                    for(DataSnapshot objSnapshot : snapshot.getChildren()){
+                        Paciente p = objSnapshot.getValue(Paciente.class);
+                        listaPaciente.add(p);
+                        if(getContext() != null){
+                            arrayAdapter = new ArrayAdapter<Paciente>(getContext(), android.R.layout.simple_list_item_1, listaPaciente);
+                            lvPacientes.setAdapter(arrayAdapter);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
     }
 }
